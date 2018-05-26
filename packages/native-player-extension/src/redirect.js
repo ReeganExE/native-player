0ee05a95e56b08c9c0a58383ef219b45add215dc
ReeganExE <dongian.rapclubkhtn@gmail.com>
2018-05-26 20:21:01 +0700

    Display file name on the redirected page

--- packages/native-player-extension/src/redirect.js
+++ packages/native-player-extension/src/redirect.js
@@ -3,6 +3,7 @@ import { parse } from 'query-string';
 document.addEventListener('DOMContentLoaded', () => {
   const { url } = parse(window.location.search);
   Object.assign(document.getElementById('link'), { href: url, textContent: url });
+  document.title = decodeURIComponent(url).split('/').pop();
   document.getElementById('close').addEventListener('click', () => {
     window.close();
   });
